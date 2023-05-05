e41eeff64831d7e636b6bad409226e43044373cd
DanteBartel <dantedbartel@gmail.com>
2023-05-05 11:38:41 +0700

    Nhan CP 230505 11:38

--- app/src/main/java/Demo/Android/WorkingSessionActivity.java
+++ app/src/main/java/Demo/Android/WorkingSessionActivity.java
@@ -1,26 +1,36 @@
 package Demo.Android;
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+import java.util.TimeZone;
 import java.util.Timer;
 import java.util.TimerTask;
 
 
 public class WorkingSessionActivity extends AppCompatActivityExtended {
+    ListView list_view;
+    List<SessionData> dataList;
+    SessionAdapter adapter;
     Button logout;
-    //TextView countdown;
     int timeSelect = 0;
     int timeProcess = 0;
     long pauseOfset = 0;
@@ -28,30 +38,14 @@ public class WorkingSessionActivity extends AppCompatActivityExtended {
     private final int ID_ACCOUNT = 2;
     private final int ID_NOTE = 3;
     private final int ID_SETTING = 4;
-    ProgressBar progressBar;
-    CountDownTimer timeCoundown;
-    ImageButton addbtn,resetBtn;
+    CountDownTimer timeCountdown;
+    ImageButton addbtn, resetBtn;
     Button startBtn;
-    TextView addTimetv;
-    boolean isStart = true;
- //   Timer timer = new Timer();
-//    TimerTask timerTask = new TimerTask() {
-//        int progress = 15;
-//        @Override
-//        public void run() {
-//            if (progress > 0) {
-//                progress--;
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        progressBar.setProgress(progress);
-//                        countdown.setText(String.valueOf(progress));
-//                    }
-//                });
-//            }
-//        }
-//    };
-
+    TextView timeLeftTv, sessionStatus;
+    ProgressBar progressBar;
+    boolean isRunning = false;
+    SimpleDateFormat intervalFormat = new SimpleDateFormat("HH:mm");
+    TimeZone gmt = TimeZone.getTimeZone("GMT");
     private WebSocketManager webSocketManager;
 
     @Override
@@ -59,18 +53,39 @@ public class WorkingSessionActivity extends AppCompatActivityExtended {
         // ---------------- Init
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workingsession);
+
+        // ---------------- Create object to handle button
         addbtn = (ImageButton) findViewById(R.id.btnAdd);
         logout = (Button) findViewById(R.id.logout);
 
         startBtn = (Button) findViewById(R.id.btnPlayPause);
         resetBtn = (ImageButton) findViewById(R.id.ib_reset);
-       // addTimetv = (TextView)findViewById(R.id.tv_addTime);
-        addbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setTimeFunction();
-            }
-        });
+        timeLeftTv = (TextView) findViewById(R.id.tvTimeLeft);
+        sessionStatus = (TextView) findViewById(R.id.sessionStatus);
+        progressBar = findViewById(R.id.pbTimer);
+        intervalFormat.setTimeZone(gmt);
+
+        list_view = findViewById(R.id.list_view);
+
+        // ---------------- Create Websocket
+        webSocketManager = new WebSocketManager(WorkingSessionActivity.this);
+        webSocketManager.start();
+
+        // ---------------- Init View
+
+        // ---------------- Init List
+        dataList = new ArrayList<>();
+        dataList.add(new SessionData("Study", "00:01"));
+        dataList.add(new SessionData("Rest", "00:01"));
+        adapter = new SessionAdapter(this, dataList);
+        list_view.setAdapter(adapter);
+        list_view.setChoiceMode(ListView.CHOICE_MODE_NONE);
+
+        // ---------------- Init timeCountdown
+        resetTime();
+        initTimeCountDown();
+
+        // ---------------- Init listener
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -80,102 +95,79 @@ public class WorkingSessionActivity extends AppCompatActivityExtended {
         resetBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                resetTime();
+                skip();
             }
         });
-//        addTimetv.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                addExtraTime();
-//            }
-//        });
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 LogOut();
             }
         });
-        // ---------------- Create Websocket
-        webSocketManager = new WebSocketManager(WorkingSessionActivity.this);
-        webSocketManager.start();
-
-        // ---------------- Init View
-
-
-
-        // ---------------- Init listener
-
-//        startSession.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                timer.schedule(timerTask, 0, 1000);
-//            }
-//        });
-        MeowBottomNavigation bottomNavigation = findViewById(R.id.bottomNavigation);
-        bottomNavigation.add(new MeowBottomNavigation.Model(ID_HOME,R.drawable.baseline_home_40));
-        bottomNavigation.add(new MeowBottomNavigation.Model(ID_ACCOUNT,R.drawable.baseline_person_24));
-        bottomNavigation.add(new MeowBottomNavigation.Model(ID_NOTE,R.drawable.baseline_notifications_24));
-        bottomNavigation.add(new MeowBottomNavigation.Model(ID_SETTING,R.drawable.baseline_settings_24));
-        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener(){
-            @Override
-            public void onClickItem(MeowBottomNavigation.Model item){
-                Toast.makeText(WorkingSessionActivity.this,"Click item : "+item.getId(),Toast.LENGTH_SHORT).show();
-            }
-        });
-        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
-            @Override
-            public void onShowItem(MeowBottomNavigation.Model item) {
-                String name;
-                switch (item.getId()){
-                    case ID_HOME:
-                        name = "home";
-                        break;
-                    case ID_ACCOUNT:
-                        name = "account";
-                        break;
-                    case ID_NOTE:
-                        name = "notification";
-                        break;
-                    case ID_SETTING:
-                        name = "setting";
-                        break;
-                    default:
-                        name="";
-                        break;
-                }
-            }
-        });
-
-        //Dùng chức năng này phát triển module 2 - Cảnh báo giá trị vượt ngưỡng
-        bottomNavigation.setCount(ID_NOTE,"4");
-        bottomNavigation.show(ID_HOME,true);
     }
-
-//    @Override
-//    protected void onDestroy() {
-//        super.onDestroy();
-//        timer.cancel();
-//        timerTask.cancel();
-//    }
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(timeCoundown!=null)
+        if(timeCountdown!=null)
         {
-            timeCoundown.cancel();
+            timeCountdown.cancel();
             timeProcess=0;
         }
     }
     //  ---------------- Addition Method
+    @SuppressLint("SetTextI18n")
+    private void initTimeCountDown() {
+        // Pop the first item of list
+        if (dataList.size() == 0) return;
+        SessionData objectData = dataList.get(0);
+        String sessionStatusString = objectData.getType();
+        String sessionIntervalString = objectData.getInterval();
+        Log.w("WorkingSessionActivity", sessionIntervalString);
+        dataList.remove(0);
+        adapter.notifyDataSetChanged();
+        // Set view items and number
+        int totalSeconds = 0;
+        try {
+            Date interval = intervalFormat.parse(sessionIntervalString);
+            totalSeconds = (int) (interval.getTime() / 1000);
+            Log.w("WorkingSessionActivity", String.valueOf(totalSeconds));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        int totalMinutes = totalSeconds / 60;
+        Log.w("WorkingSessionActivity", "totalMinutes: " + String.valueOf(totalMinutes));
+        String minuteString = Integer.toString(totalMinutes) + ":00";
+        Log.w("WorkingSessionActivity", "timeLeftTv: " + minuteString);
+        int finalTotalSeconds = totalSeconds;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                timeProcess = 0;
+                pauseOfset = 0;
+                sessionStatus.setText(sessionStatusString);
+                timeLeftTv.setText(minuteString);
+                startBtn.setText("Start");
+                timeSelect = finalTotalSeconds;
+                progressBar.setMax(timeSelect);
+            }
+        });
+        // Run Countdown if it is running status
+        Log.w("WorkingSessionActivity", "isRunning: " + String.valueOf(isRunning));
+        if (isRunning) {
+            Log.w("WorkingSessionActivity", "Countdown continue");
+            startBtn.setText("Pause");
+            startTimer(pauseOfset);
+        }
+    }
     private void startTimerSetup() {
         Button startBtn = findViewById(R.id.btnPlayPause);
         if (timeSelect > timeProcess) {
-            if (isStart) {
+            if (!isRunning) {
                 startBtn.setText("Pause");
                 startTimer(pauseOfset);
-                isStart = false;
+                isRunning = true;
             } else {
-                isStart = true;
+                isRunning = false;
                 startBtn.setText("Resume");
                 timePause();
             }
@@ -183,96 +175,72 @@ public class WorkingSessionActivity extends AppCompatActivityExtended {
             Toast.makeText(this, "Enter Time", Toast.LENGTH_SHORT).show();
         }
     }
+    private void timePause()
+    {
+        if (timeCountdown!=null)
+        {
+            timeCountdown.cancel();
+        }
+    }
+    private void startTimer(long pauseOffSetL) {
+        ProgressBar progressBar = findViewById(R.id.pbTimer);
+        progressBar.setProgress(timeProcess);
+        timeCountdown = new CountDownTimer((timeSelect * 1000L) - pauseOffSetL * 1000L, 1000) {
+            @Override
+            public void onTick(long p0) {
+                timeProcess++;
+                pauseOfset = timeSelect - p0 / 1000;
+                progressBar.setProgress(timeSelect - timeProcess);
+                TextView timeLeftTv = findViewById(R.id.tvTimeLeft);
+                timeLeftTv.setText(String.valueOf((timeSelect - timeProcess)/60)+":"+String.valueOf((timeSelect - timeProcess)%60));
+            }
+            @Override
+            public void onFinish() {
+                Toast.makeText(WorkingSessionActivity.this, "Finish one session!", Toast.LENGTH_SHORT).show();
+                initTimeCountDown();
+            }
+        }.start();
+    }
+    private void resetTime() {
+        if (timeCountdown != null) {
+            timeCountdown.cancel();
+            timeProcess = 0;
+            timeSelect = 0;
+            pauseOfset = 0;
+            timeCountdown = null;
+            startBtn.setText("Start");
+            isRunning = false;
+            progressBar.setProgress(0);
+            timeLeftTv.setText("0");
+        }
+    }
+    private void skip() {
+        if (timeCountdown != null) {
+            timeCountdown.cancel();
+            timeCountdown = null;
+        }
+        if (dataList.size() == 0) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    startBtn.setText("Start");
+                    isRunning = false;
+                    progressBar.setProgress(0);
+                    timeLeftTv.setText("0");
+                }
+            });
+        } else {
+            initTimeCountDown();
+        }
 
+    }
+    public void parsingSessionList() {
+
+    }
     public void LogOut() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         webSocketManager.closeSocket();
         finish();
     }
-    private void setTimeFunction() {
-        final Dialog timeDialog = new Dialog(this);
-        timeDialog.setContentView(R.layout.add_dialog);
-        final EditText timeSet = timeDialog.findViewById(R.id.etGetTime);
-        final TextView timeLeftTv = findViewById(R.id.tvTimeLeft);
-        final Button btnStart = findViewById(R.id.btnPlayPause);
-        final ProgressBar progressBar = findViewById(R.id.pbTimer);
-
-        timeDialog.findViewById(R.id.btnOk).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (timeSet.getText().toString().isEmpty()) {
-                    Toast.makeText(WorkingSessionActivity.this, "Enter Time Duration", Toast.LENGTH_SHORT).show();
-                } else {
-                    resetTime();
-                    timeLeftTv.setText(timeSet.getText().toString());
-                    btnStart.setText("Start");
-                    timeSelect = Integer.parseInt(timeSet.getText().toString());
-                    progressBar.setMax(timeSelect);
-                }
-                timeDialog.dismiss();
-            }
-        });
-        timeDialog.show();
-    }
-
-    private void timePause()
-    {
-        if (timeCoundown!=null)
-        {
-            timeCoundown.cancel();
-        }
-    }
-
-    private void startTimer(long pauseOffSetL) {
-        ProgressBar progressBar = findViewById(R.id.pbTimer);
-        progressBar.setProgress(timeProcess);
-        timeCoundown = new CountDownTimer((timeSelect * 1000L) - pauseOffSetL * 1000L, 1000) {
-            @Override
-            public void onTick(long p0) {
-                timeProcess++;
-                pauseOfset = timeSelect - p0 / 1000;
-
-                progressBar.setProgress(timeSelect - timeProcess);
-
-
-                TextView timeLeftTv = findViewById(R.id.tvTimeLeft);
-                timeLeftTv.setText(String.valueOf((timeSelect - timeProcess)/60)+":"+String.valueOf((timeSelect - timeProcess)%60));
-            }
-
-            @Override
-            public void onFinish() {
-                resetTime();
-                Toast.makeText(WorkingSessionActivity.this, "Times Up!", Toast.LENGTH_SHORT).show();
-            }
-        }.start();
-    }
-    private void resetTime() {
-        if (timeCoundown != null) {
-            timeCoundown.cancel();
-            timeProcess = 0;
-            timeSelect = 0;
-            pauseOfset = 0;
-            timeCoundown = null;
-            Button startBtn = findViewById(R.id.btnPlayPause);
-            startBtn.setText("Start");
-            isStart = true;
-            ProgressBar progressBar = findViewById(R.id.pbTimer);
-            progressBar.setProgress(0);
-            TextView timeLeftTv = findViewById(R.id.tvTimeLeft);
-            timeLeftTv.setText("0");
-        }
-    }
-    private void addExtraTime() {
-        ProgressBar progressBar = findViewById(R.id.pbTimer);
-        if (timeSelect!= 0) {
-            timeSelect += 15;
-            progressBar.setMax(timeSelect);
-            timePause();
-            startTimer(pauseOfset);
-            Toast.makeText(this, "15 seconds added", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-
 }
